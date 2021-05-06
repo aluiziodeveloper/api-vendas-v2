da9f863df182ff8a8ff5b6ac63f495f0d3280b1e
Aluizio Developer <aluiziodeveloper@gmail.com>
2021-05-06 17:09:41 -0300

    Correcao a interface IProduct

--- src/modules/products/domain/models/IProduct.ts
+++ src/modules/products/domain/models/IProduct.ts
@@ -2,7 +2,7 @@ import { IOrderProducts } from '@modules/orders/domain/models/IOrderProducts';
 
 export interface IProduct {
   id: string;
-  order_products: IOrderProducts[];
+  order_products?: IOrderProducts[];
   name: string;
   price: number;
   quantity: number;
