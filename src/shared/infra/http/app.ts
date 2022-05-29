9bf1a9b8df226055deae44117e21796d68fc2943
Aluizio Developer <aluiziodeveloper@gmail.com>
2022-05-29 18:00:31 -0300

    Config server with DataSource

---
+++ src/shared/infra/http/app.ts
@@ -0,0 +1,45 @@
+import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
+import cors from 'cors';
+import { errors } from 'celebrate';
+import { pagination } from 'typeorm-pagination';
+import routes from './routes';
+import AppError from '@shared/errors/AppError';
+import '@shared/infra/typeorm';
+import '@shared/container';
+import uploadConfig from '@config/upload';
+import rateLimiter from '@shared/infra/http/middlewares/rateLimiter';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use(rateLimiter);
+
+app.use(pagination);
+
+app.use('/files', express.static(uploadConfig.directory));
+app.use(routes);
+
+app.use(errors());
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    console.log(error);
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
+);
+
+export { app };
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,49 +1,10 @@
 import 'reflect-metadata';
 import 'dotenv/config';
-import express, { NextFunction, Request, Response } from 'express';
-import 'express-async-errors';
-import cors from 'cors';
-import { errors } from 'celebrate';
-import { pagination } from 'typeorm-pagination';
-import routes from './routes';
-import AppError from '@shared/errors/AppError';
-import '@shared/infra/typeorm';
-import '@shared/container';
-import uploadConfig from '@config/upload';
-//import rateLimiter from '@shared/infra/http/middlewares/rateLimiter';
+import { app } from './app';
+import { dataSource } from '../typeorm';
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-// app.use(rateLimiter);
-
-app.use(pagination);
-
-app.use('/files', express.static(uploadConfig.directory));
-app.use(routes);
-
-app.use(errors());
-
-app.use(
-  (error: Error, request: Request, response: Response, next: NextFunction) => {
-    if (error instanceof AppError) {
-      return response.status(error.statusCode).json({
-        status: 'error',
-        message: error.message,
-      });
-    }
-
-    console.log(error);
-
-    return response.status(500).json({
-      status: 'error',
-      message: 'Internal server error',
-    });
-  },
-);
-
-app.listen(process.env.PORT || 3333, () => {
-  console.log(`Server started on port ${process.env.PORT || 3333}! 🏆`);
+dataSource.initialize().then(() => {
+  const server = app.listen(process.env.PORT || 3333, () => {
+    console.log(`Server started on port ${process.env.PORT || 3333}! 🏆`);
+  });
 });
